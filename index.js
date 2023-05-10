ff1582655c904d35717743f4e9d8f097ab99c7e7
Cyrus2401 <cyrushessou@gmail.com>
2023-05-10 15:29:19 +0100

    add new style ; country and their capitals

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // DEfinition du tableau à 2 dimensions contenant les pays et leur capital
 let capitalAndPays =
     [
-        ["Quelle est la capitale du Benin ?", "Porto-Novo"],
+        ["Quelle est la capitale du Bénin ?", "Porto-Novo"],
 
         ["Quelle est la capitale du Togo ?", "Lomé"],
 
         ["Quelle est la capitale du Ghana ?", "Accra"],
 
-        ["Quelle est la capitale de la Cote-d'Ivoire ?", "Yamoussoukro"],
+        ["Quelle est la capitale de la Côte-d'Ivoire ?", "Yamoussoukro"],
 
         ["Quelle est la capitale du Sénégal ?", "Dakar"],
 
-        ["Quelle est la capitale de la Djibouti ?", "Djibouti"],
+        ["Quelle est la capitale de Djibouti ?", "Djibouti"],
 
-        ["Quelle est la capitale de l'Haiti ?", "Port au Prince"],
+        ["Quelle est la capitale d'Haïti ?", "Port au Prince"],
 
         ["Quelle est la capitale de la France ?", "Paris"],
 
@@ -29,35 +29,35 @@ let capitalAndPays =
 
         ["Quelle est la capitale de la Tunisie ?", "Tunis"],
         
- 	["Quelle est la capitale de l'Afrique du Sud ?", "Pretoria"], 
- 	
- 	["Quelle est la capitale de l'Allemagne ?", "Berlin"], 
- 	
- 	["Quelle est la capitale de l'Angola ?", "Luanda"], 
- 	
- 	["Quelle est la capitale de l'Argentine ?", "Buenos-Aires"], 
- 	
- 	["Quelle est la capitale de l'Arménie ?", "Erevan"], 
- 	
- 	["Quelle est la capitale de l'Arabie Saoudite ?", "Riyad"], 
- 	
- 	["Quelle est la capitale de la Belgique ?", "Bruxelles"],
- 	
- 	["Quelle est la capitale du Burkina-Faso ?", "Ouagadougou"],
- 	
- 	["Quelle est la capitale du Cameroun ?", "Yaoundé"],
- 	
- 	["Quelle est la capitale du Canada ?", "Ottawa"],
- 	
- 	["Quelle est la capitale du Cap-Vert ?", "Praia"],
- 	
- 	["Quelle est la capitale de la Chine ?", "Pékin"],
- 	
- 	["Quelle est la capitale de la Colombie ?", "Bogota"],
- 	
- 	["Quelle est la capitale de la Croatie ?", "Zagreb"],
- 	
- 	["Quelle est la capitale du Congo ?", "Brazaville"],
+        ["Quelle est la capitale de l'Afrique du Sud ?", "Pretoria"], 
+        
+        ["Quelle est la capitale de l'Allemagne ?", "Berlin"], 
+        
+        ["Quelle est la capitale de l'Angola ?", "Luanda"], 
+        
+        ["Quelle est la capitale de l'Argentine ?", "Buenos-Aires"], 
+        
+        ["Quelle est la capitale de l'Arménie ?", "Erevan"], 
+        
+        ["Quelle est la capitale de l'Arabie Saoudite ?", "Riyad"], 
+        
+        ["Quelle est la capitale de la Belgique ?", "Bruxelles"],
+        
+        ["Quelle est la capitale du Burkina-Faso ?", "Ouagadougou"],
+        
+        ["Quelle est la capitale du Cameroun ?", "Yaoundé"],
+        
+        ["Quelle est la capitale du Canada ?", "Ottawa"],
+        
+        ["Quelle est la capitale du Cap-Vert ?", "Praia"],
+        
+        ["Quelle est la capitale de la Chine ?", "Pékin"],
+        
+        ["Quelle est la capitale de la Colombie ?", "Bogota"],
+        
+        ["Quelle est la capitale de la Croatie ?", "Zagreb"],
+        
+        ["Quelle est la capitale du Congo ?", "Brazaville"],
 
         ["Quelle est la capitale de l'Egypte ?", "Le Caire"],
 
@@ -73,9 +73,9 @@ let capitalAndPays =
         
         ["Quelle est la capitale de l'Irak ?", "Bagdad"],
 
-        ["Quelle est la capitale de l'Israel ?", "Jérusalem"],
+        ["Quelle est la capitale de l'Israël ?", "Jérusalem"],
 
-        ["Quelle est la capitale de la Jamaique ?", "Kingston"],
+        ["Quelle est la capitale de la Jamaïque ?", "Kingston"],
 
         ["Quelle est la capitale du Kenya ?", "Nairobi"],
 
@@ -85,8 +85,139 @@ let capitalAndPays =
 
         ["Quelle est la capitale du Vatican ?", "Vatican"],
 
-        ["Quelle est la capitale de l'Ukraine ?", "Kiev"]
+        ["Quelle est la capitale de l'Ukraine ?", "Kiev"],
 
+        ["Quelle est la capitale de l'Afghanistan ?", "Kaboul"],
+
+        ["Quelle est la capitale de l'Albanie ?", "Tirana"],
+
+        ["Quelle est la capitale de l'Australie ?", "Canberra"],
+
+        ["Quelle est la capitale de l'Autriche ?", "Vienne"],
+
+        ["Quelle est la capitale du Bangladesh ?", "Dacca"],
+
+        ["Quelle est la capitale de la Biélorussie ?", "Minsk"],
+
+        ["Quelle est la capitale de la Bolivie ?", "Sucre"],
+
+        ["Quelle est la capitale de la Bosnie-Herzégovine ?", "Sarajevo"],
+
+        ["Quelle est la capitale de la Bulgarie ?", "Sofia"],
+
+        ["Quelle est la capitale du Danemark ?", "Copenhague"],
+
+        ["Quelle est la capitale de l'Equateur ?", "Quito"],
+
+        ["Quelle est la capitale de l'Ethiopie ?", "Addis-Abeba"],
+
+        ["Quelle est la capitale de la Finlande ?", "Helsinki"],
+
+        ["Quelle est la capitale de la Gambie ?", "Banjul"],
+
+        ["Quelle est la capitale du Guatemala ?", "Guatemala"],
+
+        ["Quelle est la capitale de la Guinée ?", "Conakry"],
+
+        ["Quelle est la capitale de la Guinée équatoriale ?", "Malabo"],
+
+        ["Quelle est la capitale de la Guinée-Bissau ?", "Bissau"],
+
+        ["Quelle est la capitale de la Hongrie ?", "Budapest"],
+
+        ["Quelle est la capitale de l'Iran ?", "Téhéran"],
+
+        ["Quelle est la capitale de l'Irlande ?", "Dublin"],
+
+        ["Quelle est la capitale du Kazakhstan ?", "Astana"],
+
+        ["Quelle est la capitale de la Lettonie ?", "Riga"],
+
+        ["Quelle est la capitale du Lesotho ?", "Maseru"],
+
+        ["Quelle est la capitale du Madagascar ?", "Antananarivo"],
+
+        ["Quelle est la capitale de la Malaisie ?", "Kuala Lumpur"],
+
+        ["Quelle est la capitale du Mali ?", "Bamako"],
+
+        ["Quelle est la capitale du Maroc ?", "Rabat"],
+
+        ["Quelle est la capitale de la Mauritanie ?", "Nouakchott"],
+
+        ["Quelle est la capitale du Mexique ?", "Mexico"],
+
+        ["Quelle est la capitale du Mozambique ?", "Maputo"],
+
+        ["Quelle est la capitale de la Namibie ?", "Windhoek"],
+
+        ["Quelle est la capitale du Népal ?", "Katmandou"],
+
+        ["Quelle est la capitale du Niger ?", "Niamey"],
+
+        ["Quelle est la capitale du Nigéria ?", "Abuja"],
+
+        ["Quelle est la capitale de la Norvège ?", "Oslo"],
+
+        ["Quelle est la capitale de la Nouvelle-Zélande ?", "Wellington"],
+
+        ["Quelle est la capitale de l'Ouganda ?", "Kampala"],
+
+        ["Quelle est la capitale de la Palestine ?", "Jérusalem-Est"],
+        
+        ["Quelle est la capitale du Panama ?", "Panama"],
+
+        ["Quelle est la capitale du Pays-Bas ?", "Amsterdam"],
+
+        ["Quelle est la capitale de la Philippines ?", "Manille"],
+
+        ["Quelle est la capitale du Qatar ?", "Doha"],
+
+        ["Quelle est la capitale de la République Centrafricaine ?", "Bangui"],
+
+        ["Quelle est la capitale de la République Démocratique du Congo ?", "Kinshasa"],
+
+        ["Quelle est la capitale de la Roumanie ?", "Bucarest"],
+
+        ["Quelle est la capitale du Royaume-Uni ?", "Londres"],
+
+        ["Quelle est la capitale de la Russie ?", "Moscou"],
+
+        ["Quelle est la capitale du Saint-Marin' ?", "Saint-Marin"],
+
+        ["Quelle est la capitale de la Serbie ?", "Belgrade"],
+
+        ["Quelle est la capitale de la Sierra Leone ?", "Freetown"],
+
+        ["Quelle est la capitale du Singapour ?", "Singapour"],
+
+        ["Quelle est la capitale de la Somalie ?", "Mogadiscio"],
+
+        ["Quelle est la capitale du Soudan ?", "Khartoum"],
+
+        ["Quelle est la capitale du Soudan du Sud ?", "Khartoum"],
+
+        ["Quelle est la capitale de la Suède ?", "Stockholm"],
+
+        ["Quelle est la capitale de la Suisse ?", "Berne"],
+
+        ["Quelle est la capitale de la Syrie ?", "Damas"],
+
+        ["Quelle est la capitale de la Tanzanie ?", "Dodoma"],
+
+        ["Quelle est la capitale du Tchad ?", "N'Djamena"],
+
+        ["Quelle est la capitale de la Thaïlande ?", "Bangkok"],
+
+        ["Quelle est la capitale de la Turquie ?", "Ankara"],
+
+        ["Quelle est la capitale de l'Uruguay ?", "Montevideo"],
+
+        ["Quelle est la capitale de la Zambie ?", "Sanaa"],
+
+        ["Quelle est la capitale du Yémen ?", "Ankara"],
+
+        ["Quelle est la capitale du Viêt Nam ?", "Hanoi"]
  	
     ]
 
@@ -155,14 +286,14 @@ validAnswer.addEventListener('click', function (e) {
     if (inputAnswer.value.toUpperCase().trim() == capital.toUpperCase()) {
         resultat += 1
         divShowMessage.style.display = "inherit"
-        showResult(1)
+        showResult(1, '')
         setTimeout(hideDiv, 1500)
     }
     else {
         resultat
         divShowMessage.style.display = "inherit"
-        showResult(2)
-        setTimeout(hideDiv, 1500)
+        showResult(2, capital)
+        setTimeout(hideDiv, 2500)
     }
 
     if (inputAnswer.value.trim() != "") {
@@ -196,18 +327,18 @@ validAnswer.addEventListener('click', function (e) {
 
             if (resultat < (NOMBRE_OF_QUESTIONS / 2)) {
                 img.setAttribute('src', 'mauvaisResulat.jpg')
-                affichAnswer.innerHTML = "Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS + "<br><span>Vous n'avez pas atteint la moyenne</span>"
+                affichAnswer.innerHTML = "Dommage ! Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS 
                 affichAnswer.style.color = "red"
             }
             else if (resultat >= (NOMBRE_OF_QUESTIONS / 2)) {
                 img.setAttribute('src', 'bonResultat.jpg')
-                affichAnswer.innerHTML = "Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS + "<br><span>Félicitatiion! vous avez atteint au moins la moyenne</span>"
+                affichAnswer.innerHTML = "Félicitaion ! Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS
                 affichAnswer.style.color = "green"
             }
 
             if (resultat == NOMBRE_OF_QUESTIONS) {
                 img.setAttribute('src', 'tresBonResultat.jpg')
-                affichAnswer.innerHTML = "Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS + "<br><span>Félicitatiion! vous avez atteint la note maximale</span>"
+                affichAnswer.innerHTML = "Wouaaaoooh!!! Vous avez obenu la note maximale qui est de " + resultat + "/" + NOMBRE_OF_QUESTIONS
                 affichAnswer.style.color = "green"
             }
         }
@@ -220,13 +351,6 @@ validAnswer.addEventListener('click', function (e) {
 })
 
 passQuestion.addEventListener('click', () => {
-
-    if (inputAnswer.value.toUpperCase().trim() == capital.toUpperCase()) {
-        resultat += 1
-    }
-    else {
-        resultat
-    }
 
     if (inputAnswer.value.trim() == '') {
 
@@ -259,18 +383,18 @@ passQuestion.addEventListener('click', () => {
 
             if (resultat < (NOMBRE_OF_QUESTIONS / 2)) {
                 img.setAttribute('src', 'mauvaisResulat.jpg')
-                affichAnswer.innerHTML = "Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS + "<br><span>Vous n'avez pas atteint la moyenne</span>"
+                affichAnswer.innerHTML = "Dommage ! Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS 
                 affichAnswer.style.color = "red"
             }
             else if (resultat >= (NOMBRE_OF_QUESTIONS / 2)) {
                 img.setAttribute('src', 'bonResultat.jpg')
-                affichAnswer.innerHTML = "Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS + "<br><span>Félicitatiion! vous avez atteint au moins la moyenne</span>"
+                affichAnswer.innerHTML = "Félicitaion ! Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS
                 affichAnswer.style.color = "green"
             }
 
             if (resultat == NOMBRE_OF_QUESTIONS) {
                 img.setAttribute('src', 'tresBonResultat.jpg')
-                affichAnswer.innerHTML = "Vous avez obenu une note de " + resultat + "/" + NOMBRE_OF_QUESTIONS + "<br><span>Félicitatiion! vous avez atteint la note maximale</span>"
+                affichAnswer.innerHTML = "Wouaaaoooh !!! Vous avez obenu la note maximale qui est de " + resultat + "/" + NOMBRE_OF_QUESTIONS
                 affichAnswer.style.color = "green"
             }
         }
@@ -287,7 +411,6 @@ function hideDiv() {
 }
 
 function showMistake(nbr) {
-
     switch (nbr) {
         case 1:
             message.innerHTML = "Veuiller entrer une réponse pour valider"
@@ -304,8 +427,7 @@ function showMistake(nbr) {
     }
 }
 
-function showResult(nbr) {
-
+function showResult(nbr, goodAnswer) {
     switch (nbr) {
         case 1:
             message.innerHTML = "Bonne réponse"
@@ -313,7 +435,7 @@ function showResult(nbr) {
             break
 
         case 2:
-            message.innerHTML = "Mauvaise réponse"
+            message.innerHTML = "Mauvaise réponse ! La bonne réponse est <strong style='font-weight: 800'>" + goodAnswer + "</strong>"
             divShowMessage.style.backgroundColor = "red"
             break
 
